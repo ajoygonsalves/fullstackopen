a6a5a7927f7147f9b7b816da2721a490a5ecb9e6
ajoyous <ajoy.gonsalves@hotmail.com>
2024-06-04 22:29:26 +0200

    build: Exercises 1.1.-1.2.

---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Part from "./Part";
+
+export default function Content({
+  part1,
+  part2,
+  part3,
+  exercises1,
+  exercises2,
+  exercises3,
+}) {
+  return (
+    <>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+    </>
+  );
+}
